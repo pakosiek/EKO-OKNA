4a26c627a36bf2f0260cad3c53324e53ee9ab37a
Kajetan Dąbek <268841@student.pwr.edu.pl>
2022-05-18 21:22:34 +0200

    Część __init__ została skończona

--- MDBase_init_.py
+++ MDBase_init_.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
 import struct 
+from __future__ import annotations
+from abc import abstractclassmethod
+from typing import Callable, Tuple
+
 
 class MDBase:
 
-    block_size = 64
 
     def __init__(self, txt: str ="",form_file: bool = False):
         
-        self.from_file = from_file
-        self.txt = txt
-        if from_file = True:
-            # ewentualnie tutaj będzie wywołanie metody
-            text = open(txt, "r")
-
-        else:
-            
+        self.is_from_file = from_file
+        self.content = txt
         
-        _a0 = 0x67452301
-        _b0 = 0xEFCDAB89
-        _c0 = 0x98BADCFE
-        _d0 = 0x10325476
+
+            
+        self._a0 = 0x67452301
+        self._b0 = 0xEFCDAB89
+        self._c0 = 0x98BADCFE
+        self._d0 = 0x10325476
         
