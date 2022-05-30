5351915815d045a5dac31b1d8a268553cc7d3817
Kajetan Dąbek <268841@student.pwr.edu.pl>
2022-05-30 14:11:37 +0200

    "Dodałem szkic MD4"

--- mdclasses.py
+++ mdclasses.py
@@ -29,3 +29,17 @@ class MDBase:
     def _code() -> str:
         pass
     
+
+class MD4 :
+    def __init__(self, txt):
+        self.txt_encode = txt.encode()
+        n = len(self.txt_encode) * 8        
+
+        #super używamy dopiero przy algorytmie
+        #tutaj ta część z lib struct, to maksym zrób
+
+
+        n_extended = len(self.msg_encode)
+
+        self.chunks = [self.txt_encode[i:i+n_extended//8] for i in range(0, len(self.txt_encode), n_extended//8)]
+        #prawdopodobie trzeba jeszcze zaimportowaća0,b0,c0,d0, ale to adrian musi potwierdzić czy tak chce, bo będzie narzekał na ram
