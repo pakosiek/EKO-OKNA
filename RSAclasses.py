8d9c6d79ab87ea449770c4b3647c17d5c689013e
Adrian Krawczyk <268855@student.pwr.edu.pl>
2022-06-04 00:05:23 +0200

    obiecuje ostatnia poprawka, diagram + init rsa

--- RSAclasses.py
+++ RSAclasses.py
@@ -17,13 +17,15 @@ class MessageToLarge(Exception):
         return "Message is larger than n!"
 
 class rsa:
-    def __init__(self, primes: Tuple[int, int] = None) -> None:
+    def __init__(self, primes: Tuple[int, int] = None, prime_len = None) -> None:
         (p, q) = (None, None)
         if (primes):
             if (not self.__is_prime(primes[0])): raise IsNotPrime
             if (not self.__is_prime(primes[1])): raise IsNotPrime
             (p, q) = primes
-        else: (p, q) = self.__find_primes()       
+        else: 
+            if (prime_len): (p, q) = self.__find_primes(prime_len)
+            else: (p, q) = self.__find_primes()
         self.__n = p * q
         phi = (p - 1) * (q - 1)
         e = random.randrange(1, phi)
@@ -31,8 +33,8 @@ class rsa:
         while gcd != 1:
             e = random.randrange(1, phi)
             gcd = math.gcd(e, phi)
-        self.e = e
-        self.d = self.__euclides(e, phi, 1)[0] % phi   
+        self.__e = e
+        self.__d = self.__euclides(e, phi, 1)[0] % phi   
 
 #                              _____     ____
 #                            /      \  |  o | 
