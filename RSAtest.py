9d0604ce5bda056e00ed30661c9bf504e633979e
Kajetan Dąbek <268841@student.pwr.edu.pl>
2022-06-04 19:50:13 +0200

    Wysyłam na serwer doctest RSAclasses.

---
+++ RSAtest.py
@@ -0,0 +1,40 @@
+import RSAclasses
+import doctest
+
+
+
+def y(x):
+    
+    """
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(12, a.public_key),a.private_key)
+    12
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(1324, a.public_key),a.private_key)
+    1324
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(142536475869, a.public_key),a.private_key)
+    142536475869
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(1595135753, a.public_key),a.private_key)
+    1595135753
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(314, a.public_key),a.private_key)
+    314
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(0, a.public_key),a.private_key)
+    0
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(714065201, a.public_key),a.private_key)
+    714065201
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(961870723, a.public_key),a.private_key)
+    961870723
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(56113794, a.public_key),a.private_key)
+    56113794
+    >>> RSAclasses.rsa.decode(RSAclasses.rsa.code(529816129, a.public_key),a.private_key)
+    529816129
+    RSAclasses.rsa.decode(RSAclasses.rsa.code(1029384756, a.public_key),a.private_key)
+    1029384756
+    RSAclasses.rsa.decode(RSAclasses.rsa.code(11111111111, a.public_key),a.private_key)
+    11111111111
+    RSAclasses.rsa.decode(RSAclasses.rsa.code(8008132, a.public_key),a.private_key)
+    8008132
+    """
+    return RSAclasses.rsa.decode(RSAclasses.rsa.code(x, a.public_key),a.private_key)
+
+
+
+doctest.testmod()
